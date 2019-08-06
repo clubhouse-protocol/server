a5140d7e260e93c1d9de854c1435249fe0563752
Morten Olsen <morten@olsen.pro>
2019-08-06 21:18:02 +0200

    init

---
+++ src/MessageStore/index.ts
@@ -0,0 +1,7 @@
+interface MessageStore {
+  has: (signal: string) => Promise<boolean>;
+  get: (id: string) => Promise<string | undefined>;
+  set: (id: string, value: string) => Promise<void>;
+}
+
+export default MessageStore;
---
+++ src/socket/index.ts
@@ -0,0 +1,34 @@
+import socketio from 'socket.io';
+import Client from './Client';
+import { Server } from 'http';
+import { createHash } from 'crypto';
+import MessageStore from '../MessageStore';
+
+type PromiseResolveType<Original> = Original extends Promise<infer ReturnType> ? ReturnType : never;
+
+const create = async (server: Server, store: MessageStore) => {
+  const app = socketio(server);
+  let clients: Client[] = [];
+
+  app.on('connect', (socket) => {
+    const client = new Client(socket, store);
+    clients.push(client);
+    client.on('message', (message: any) => {
+      const hash = createHash('sha512').update(message.id).digest('hex');
+      clients.forEach(c => c.handleMessage({
+        ...message,
+        signal: hash,
+      }));
+    });
+  });
+
+  return app;
+};
+
+type SocketServer = PromiseResolveType<ReturnType<typeof create>>;
+
+export {
+  SocketServer,
+};
+
+export default create;
